bfdabae8cd5303b59fe67809e4bed263eace735c
Jason Schatz <jason.schatz@udacity.com>
2016-09-13 10:49:59 -0700

    fist auto layout commit

---
+++ Auto-Layout/Text and Images/Text and Images/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Text and Images
+//
+//  Created by Jason Schatz on 9/13/16.
+//  Copyright © 2016 Jason Schatz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
